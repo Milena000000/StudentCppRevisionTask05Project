b311650c2c15000de880ce348c62f1b569c44852
Victor Ivanchenko <ivanvikvik@gmail.com>
2023-03-26 15:52:08 +0300

    Update taskX.cpp

--- StudentCppRevisionTask05Project/taskX.cpp
+++ StudentCppRevisionTask05Project/taskX.cpp
@@ -15,8 +15,26 @@
 *	Формат выходных данных [output]
 *	Функция должна вывести одно натуральное число или 0 в случае некорректных данных.
 *
-*	[Sample function input]: 10 3 2
-*	[Sample function output]: 8
+*	[ input 1]: 10 3 2
+*	[output 1]: 8
+*
+*	[ input 2]: 10 2 1
+*	[output 2]: 9
+*
+*	[ input 3]: 3 2 1
+*	[output 3]: 2
+*
+*	[ input 4]: 4 5 3
+*	[output 4]: 1
+*
+*	[ input 5]: -10 3 2
+*	[output 5]: 0
+*
+*	[ input 6]: 10 2 3
+*	[output 6]: 0
+*
+*	[ input 7]: 2 5 3
+*	[output 7]: 0
 */
 
 int taskX(int h, int a, int b) {
